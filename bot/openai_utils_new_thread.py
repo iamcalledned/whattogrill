00855395fde7ce5ae27d480dd16a681b9193dc24
Ned <iamcalledned@gmail.com>
2023-12-08 08:57:18 -0500

    Update openai_utils_new_thread.py

--- bot/openai_utils_new_thread.py
+++ bot/openai_utils_new_thread.py
@@ -1,6 +1,19 @@
 #openai_utils_new_thread.py
 from openai import OpenAI
+import sys
 import os
+# Get the directory of the current script
+current_script_path = os.path.dirname(os.path.abspath(__file__))
+# Set the path to the parent directory (one folder up)
+parent_directory = os.path.dirname(current_script_path)
+# Add the config directory to sys.path
+sys.path.append(os.path.join(parent_directory, 'database'))
+sys.path.append(os.path.join(parent_directory, 'config'))
+import config
+
+OPENAI_API_KEY = config.OPENAI_API_KEY
+
+
 
 # Initialize OpenAI client
 openai_client = OpenAI()
