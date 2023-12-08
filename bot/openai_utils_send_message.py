c603359232767be059fc586e6c8176c6c9f1db06
Ned <iamcalledned@gmail.com>
2023-12-08 08:57:10 -0500

    Update openai_utils_send_message.py

--- bot/openai_utils_send_message.py
+++ bot/openai_utils_send_message.py
@@ -1,7 +1,18 @@
 #openai_utils_send_message.py
 from openai import OpenAI
-import os
 import time
+import sys
+import os
+# Get the directory of the current script
+current_script_path = os.path.dirname(os.path.abspath(__file__))
+# Set the path to the parent directory (one folder up)
+parent_directory = os.path.dirname(current_script_path)
+# Add the config directory to sys.path
+sys.path.append(os.path.join(parent_directory, 'database'))
+sys.path.append(os.path.join(parent_directory, 'config'))
+import config
+
+OPENAI_API_KEY = config.OPENAI_API_KEY
 
 # Initialize OpenAI client
 openai_client = OpenAI()
