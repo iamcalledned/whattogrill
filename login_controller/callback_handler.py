acb7418789e869c766cb685b729570a7c74bf3ee
Ned <iamcalledned@gmail.com>
2023-12-08 13:15:23 -0500

    Update callback_handler.py

--- login_controller/callback_handler.py
+++ login_controller/callback_handler.py
@@ -7,7 +7,7 @@ current_script_path = os.path.dirname(os.path.abspath(__file__))
 parent_directory = os.path.dirname(current_script_path)
 # Add the config directory to sys.path
 sys.path.append(os.path.join(parent_directory, 'config'))
-from flask import Flask, redirect, request, session, url_for, render_template, make_response, jsonify
+from flask import Flask, redirect, request, session, url_for, render_template, make_response
 from auth import exchange_code_for_token, validate_token, generate_nonce
 import config
 import json
@@ -16,6 +16,7 @@ from page_renderer import logged_in
 import logging
 import asyncio
 import gevent
+from login_controller import get_session_data
 
 
 # Configure logging with an absolute path for the log file
@@ -28,47 +29,46 @@ logging.basicConfig(
 
 
 async def handle_callback(redis_client):
+    
     print("in callback")
+    print("redis client:", redis_client)
     code = request.args.get('code')
     print("code:", code)
+    # Retrieve session data before checking the code
+    session_data = get_session_data()
+    session_data_json = json.loads(session_data.get_data(as_text=True))
+    user_info = session_data_json.get('sessionId')
+    print("session Id from beginning of callback")
+
     if code:
         try:
             tokens = exchange_code_for_token(code)
+            print("tokens:", tokens)
             if tokens:
                 id_token = tokens['id_token']
                 decoded_token = validate_token(id_token)
-
+                
                 # Store user information in session
                 session['email'] = decoded_token.get('email', 'unknown')
                 session['username'] = decoded_token.get('cognito:username', 'unknown')
                 session['name'] = decoded_token.get('name', 'unknown')
-
-                # Generate a new session ID or retrieve the existing one
-                session_id = session.get('session_id', os.urandom(24).hex())
+                print("made it to logged in")
+                
+                # Attempt to save data to Redis
+                session_id = os.urandom(24).hex()
                 session['session_id'] = session_id
-                user_info = {
-                    'username': session['username'],
-                    'email': session['email'],
-                    'name': session['name'],
-                    'session_id': session_id
-                }
-
-                # Save or update the session data in Redis
+                user_info = {'username': session['username'], 'email': session['email'], 'name': session['name'], 'session_id': session['session_id']}
                 redis_set_result = redis_client.set(session_id, json.dumps(user_info))
-                if redis_set_result:
-                    # If the session is saved successfully, proceed to the logged_in flow
-                    return await logged_in(session, redis_client)  # This will handle existing sessions
-                else:
-                    # If saving the session fails, return an error
-                    return jsonify({'error': 'Failed to save session data'}), 500
-            else:
-                # If token exchange fails, return an error
-                return jsonify({'error': 'Error during token exchange'}), 400
+                print("redis set result:", redis_set_result)
+                print("session id from redis:", redis_client.get(session_id))
+                if not redis_set_result:
+                    return 'Failed to save data to Redis.', 500
+                print("going to logged_in")
+                return await logged_in(session, redis_client)  # Call the function from login_controler
+                #return logged_in(session, redis_client)  # Call the function from login_controler
+            else:   
+                return 'Error during token exchange.', 400
         except Exception as e:
-            # If an exception occurred, return an error
-            return jsonify({'error': f'Token validation error: {str(e)}'}), 400
-    else:
-        # If no code parameter is provided, return an error
-        return jsonify({'error': 'Authentication failed or cancelled'}), 400
+            return f"Token validation error: {str(e)}", 400
 
-# Define your logged_in function here if it's not already defined
+    return 'Authentication failed or cancelled.', 400
