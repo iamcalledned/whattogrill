a8151b68e35552897cef87d5605f25d5fde7383d
Ned <iamcalledned@gmail.com>
2023-12-08 12:18:01 -0500

    Update page_renderer.py

--- bot/page_renderer.py
+++ bot/page_renderer.py
@@ -1,5 +1,5 @@
 # page_renderer.py
-from flask import render_template, redirect, url_for, send_from_directory
+from flask import render_template, redirect, url_for, send_from_directory, render_template_string, Response
 from auth import generate_nonce
 import json
 import asyncio
@@ -23,39 +23,32 @@ redis_client = redis.Redis(host=config.REDIS_HOST, port=config.REDIS_PORT, db=0)
 
 async def logged_in(session, redis_client):
     if 'username' in session:
-        
         nonce = generate_nonce()
-        
-        # Get session_id from the current session
         session_id = session.get('session_id')
         print("SESSION ID: ", session_id)
-        
-        
+
+        user_info = {}
         if session_id:
-            # Attempt to retrieve data from Redis based on session_id
             redis_data = redis_client.get(session_id)
-            
             if redis_data:
-                # Parse the JSON data retrieved from Redis
                 user_info = json.loads(redis_data.decode('utf-8'))
-                
-                
-                # Establish a WebSocket connection asynchronously
-                #run_async_connect_to_chatbot(session_id, session['username'])
-                #print("WebSocket connection initiated")
-                
             else:
-                # Handle the case when data is not found in Redis
-                user_info = {}
                 print("Data not found in Redis for session_id:", session_id)
         else:
-            # Handle the case when session_id is not found in the current session
-            user_info = {}
             print("Session ID not found in session for user.", user_info)
 
-        
         print("USER LOGGED IN: ", user_info)
-        #return render_template('chat.html', sessionId=session_id, nonce=nonce, user_info=user_info)
-        return send_from_directory('/var/www/html', 'chat.html', sessionId=session_id, nonce=nonce, user_info=user_info)
+
+        # Path to your chat.html file
+        chat_html_path = '/var/www/html/chat.html'
+        if os.path.exists(chat_html_path):
+            with open(chat_html_path, 'r') as file:
+                chat_html_content = file.read()
+
+            # Render the HTML with dynamic data
+            rendered_content = render_template_string(chat_html_content, sessionId=session_id, nonce=nonce, user_info=user_info)
+            return Response(rendered_content, mimetype='text/html')
+        else:
+            return 'chat.html not found', 404
     else:
         return redirect(url_for('login'))
