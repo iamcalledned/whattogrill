56ab99fa6a9187bb124b7050f9f1920de75386fb
Ned <iamcalledned@gmail.com>
2023-12-11 17:19:47 -0500

    Update session_config.py

--- login_controller/session_config.py
+++ login_controller/session_config.py
@@ -1,9 +1,11 @@
+from flask import Flask
 from flask_session import Session
 import redis
 
-def init_session(app):
-    app.config['SESSION_TYPE'] = 'redis'
-    app.config['SESSION_PERMANENT'] = False
-    app.config['SESSION_USE_SIGNER'] = True
-    app.config['SESSION_REDIS'] = redis.Redis(host='localhost', port=6379, db=0)
-    Session(app)
+app = Flask(__name__)
+app.config['SESSION_TYPE'] = 'redis'
+app.config['SESSION_REDIS'] = redis.Redis(host='localhost', port=6379, db=0)
+app.config['SESSION_USE_SIGNER'] = True
+app.secret_key = 'your_secret_key'
+
+Session(app)
