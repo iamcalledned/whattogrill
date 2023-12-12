dcea876a319e518e894c8034e21886da7d423bbf
Ned <iamcalledned@gmail.com>
2023-12-12 09:56:30 -0500

    Update session_config.py

--- login_controller/session_config.py
+++ login_controller/session_config.py
@@ -6,7 +6,7 @@ from datetime import timedelta
 
 def init_session(app):
     app.config['SESSION_TYPE'] = 'redis'
-    app.config['SESSION_PERMANENT'] = False
+    app.config['SESSION_PERMANENT'] = True
     app.config['SESSION_USE_SIGNER'] = True
     app.config['SESSION_REDIS'] = redis.Redis(host='localhost', port=6378, db=0)
     app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)  # Adjust as needed
