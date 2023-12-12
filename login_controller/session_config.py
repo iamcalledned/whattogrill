3c448807a04124eff521613be975d75941ec8dc9
Ned <iamcalledned@gmail.com>
2023-12-12 08:29:23 -0500

    Update session_config.py

--- login_controller/session_config.py
+++ login_controller/session_config.py
@@ -1,10 +1,14 @@
 #session_config
 from flask_session import Session
 import redis
+from datetime import timedelta
+
 
 def init_session(app):
     app.config['SESSION_TYPE'] = 'redis'
     app.config['SESSION_PERMANENT'] = False
     app.config['SESSION_USE_SIGNER'] = True
     app.config['SESSION_REDIS'] = redis.Redis(host='localhost', port=6378, db=0)
+    app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)  # Adjust as needed
+
     Session(app)
