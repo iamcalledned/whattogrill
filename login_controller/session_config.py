74e0b990519cc9a5cfb9ec14a63452c813ed089f
Ned <iamcalledned@gmail.com>
2023-12-12 08:04:04 -0500

    Update session_config.py

--- login_controller/session_config.py
+++ login_controller/session_config.py
@@ -6,5 +6,5 @@ def init_session(app):
     app.config['SESSION_TYPE'] = 'redis'
     app.config['SESSION_PERMANENT'] = False
     app.config['SESSION_USE_SIGNER'] = True
-    app.config['SESSION_REDIS'] = redis.Redis(host='localhost', port=6379, db=0)
+    app.config['SESSION_REDIS'] = redis.Redis(host='localhost', port=6378, db=0)
     Session(app)
