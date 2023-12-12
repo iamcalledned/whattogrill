b1a90104d8ca80539e12c54c94dd8687c162bc35
Ned <iamcalledned@gmail.com>
2023-12-12 09:52:27 -0500

    Update callback_handler.py

--- login_controller/callback_handler.py
+++ login_controller/callback_handler.py
@@ -31,7 +31,7 @@ logging.basicConfig(
 
 
 async def handle_callback(redis_client):
-    
+    print("redis client", redis_client)
     print("in callback")
     code = request.args.get('code')
     print("code:", code)
@@ -39,6 +39,8 @@ async def handle_callback(redis_client):
     #traceback.print_stack()
     # Retrieve session data before checking the code
     print(f"Code Verifier Retrieved from callback: {session.get('code_verifier')}")
+    for key, value in session.items():
+        print(f"Session Key in callback: {key}, Session Value: {value}")
     if code:
         try:
             tokens = exchange_code_for_token(code)
