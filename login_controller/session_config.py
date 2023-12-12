e314e94b05acdfe677d5ff6daf1eeace1402ae22
Ned <iamcalledned@gmail.com>
2023-12-12 13:58:18 -0500

    Update session_config.py

--- login_controller/session_config.py
+++ login_controller/session_config.py
@@ -11,8 +11,8 @@ def init_session(app):
     app.config['SESSION_REDIS'] = redis.Redis(host='localhost', port=6378, db=0)
     app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)  # Adjust as needed
     app.config['SESSION_COOKIE_NAME'] = 'SessionName'
-    app.config['SESSION_COOKIE_SECURE'] = True  # Only send cookies over HTTPS
-    app.config['SESSION_COOKIE_HTTPONLY'] = True  # Prevent JavaScript access to session cookie
+    #app.config['SESSION_COOKIE_SECURE'] = True  # Only send cookies over HTTPS
+    #app.config['SESSION_COOKIE_HTTPONLY'] = True  # Prevent JavaScript access to session cookie
     app.config['SESSION_COOKIE_DOMAIN'] = '.whattogrill.com'
 
 
