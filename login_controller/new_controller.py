36c25b275fc32c0ce568c0118e7d823999557a30
Ned <iamcalledned@gmail.com>
2023-12-12 16:07:51 -0500

    Update new_controller.py

--- login_controller/new_controller.py
+++ login_controller/new_controller.py
@@ -1,22 +1,17 @@
 # new_login_controller.py
-import sys
 import os
-from flask import Flask
+from flask import Flask, redirect
 from flask_sockets import Sockets
-from flask import redirect
 from config import Config
 from asgiref.wsgi import WsgiToAsgi
-
 
 app = Flask(__name__)
 sockets = Sockets(app)
 
+# Only wrap your app with WsgiToAsgi once
 app_asgi = WsgiToAsgi(app)
 
 @app.route('/login')
 def login():
-    auth_url = f"{config.COGNITO_DOMAIN}/login?client_id={config.COGNITO_APP_CLIENT_ID}&response_type=code&scope=openid&redirect_uri={config.REDIRECT_URI}"
+    auth_url = f"{Config.COGNITO_DOMAIN}/login?client_id={Config.COGNITO_APP_CLIENT_ID}&response_type=code&scope=openid&redirect_uri={Config.REDIRECT_URI}"
     return redirect(auth_url)
-
-# In your login_controller.py
-app_asgi = WsgiToAsgi(app)
